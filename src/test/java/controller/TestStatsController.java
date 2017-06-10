3280ebaeae1ab6de2180372becbee2726e928938
Unknown <luizfzsaggioro@gmail.com>
2017-06-10 15:18:09 -0300

    Update unit test for StatsController

--- src/test/java/controller/TestStatsController.java
+++ src/test/java/controller/TestStatsController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import helper.FileHelper;
+import model.Parameters;
 import model.filefilter.StatsFileFilter;
 import org.junit.Assert;
 import org.junit.Before;
@@ -9,6 +10,7 @@ import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -34,6 +36,12 @@ public class TestStatsController {
 
     public File createLeafDirectory() throws IOException {
         File data = getRootDirectory();
+        return data;
+    }
+
+    public File createNotLeafDirectory() throws IOException{
+        File data = getRootDirectory();
+        root.newFolder(data.getName(), "data-sub1");
         return data;
     }
 
@@ -109,7 +117,116 @@ public class TestStatsController {
 
         Assert.assertTrue("Content of aggregated file is wrong",
                 readAggregateStatsFileContent.equals(aggregateStatFileContent.toString()));
+    }
 
+    @Test
+    public void shouldProcessDirectory() throws IOException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        File dir = createLeafDirectory();
+        File createdFile = File.createTempFile("abc",".txt", dir);
+        Files.write(createdFile.toPath(), STAT_VALUE.getBytes(), WRITE);
+        File createdFile2 = File.createTempFile("abd",".txt", dir);
+        Files.write(createdFile2.toPath(), STAT_VALUE2.getBytes(), WRITE);
+
+        Method methodUnderTest = statsController.getClass()
+                .getDeclaredMethod("processDirectory", File.class);
+        methodUnderTest.setAccessible(true);
+        methodUnderTest.invoke(statsController, dir);
+
+        StringBuilder aggregateStatFileContent = new StringBuilder();
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE);
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile2.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE2);
+
+        File aggregateStatFile = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.equals(dir.getName() + Parameters.STATS_FILE_EXTENSION_WITH_DOT);
+            }
+        })[0];
+
+        Method readStatFromFile = statsController.getClass()
+                .getDeclaredMethod("readStatFromFile", File.class);
+        readStatFromFile.setAccessible(true);
+        String readAggregateStatsFileContent = (String) readStatFromFile.invoke(statsController, aggregateStatFile);
+
+        Assert.assertTrue("Content of aggregated file is wrong",
+                readAggregateStatsFileContent.equals(aggregateStatFileContent.toString()));
+    }
+
+    @Test
+    public void shouldAggregateStatsOfLeafDirectory() throws IOException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        File dir = createLeafDirectory();
+        File createdFile = File.createTempFile("abc",".txt", dir);
+        Files.write(createdFile.toPath(), STAT_VALUE.getBytes(), WRITE);
+        File createdFile2 = File.createTempFile("abd",".txt", dir);
+        Files.write(createdFile2.toPath(), STAT_VALUE2.getBytes(), WRITE);
+
+        Method methodUnderTest = statsController.getClass()
+                .getDeclaredMethod("aggregateStats", File.class);
+        methodUnderTest.setAccessible(true);
+        methodUnderTest.invoke(statsController, dir);
+
+        StringBuilder aggregateStatFileContent = new StringBuilder();
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE);
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile2.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE2);
+
+        File aggregateStatFile = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.equals(dir.getName() + Parameters.STATS_FILE_EXTENSION_WITH_DOT);
+            }
+        })[0];
+
+        Method readStatFromFile = statsController.getClass()
+                .getDeclaredMethod("readStatFromFile", File.class);
+        readStatFromFile.setAccessible(true);
+        String readAggregateStatsFileContent = (String) readStatFromFile.invoke(statsController, aggregateStatFile);
+
+        Assert.assertTrue("Content of aggregated file is wrong",
+                readAggregateStatsFileContent.equals(aggregateStatFileContent.toString()));
+    }
+
+    @Test
+    public void shouldAggregateStatsOfNonLeafDirectory() throws IOException, InvocationTargetException, IllegalAccessException, NoSuchMethodException {
+        File dir = createNotLeafDirectory();
+        File subDir = dir.listFiles()[0];
+        File createdFile = File.createTempFile("abc",".txt", subDir);
+        Files.write(createdFile.toPath(), STAT_VALUE.getBytes(), WRITE);
+        File createdFile2 = File.createTempFile("abd",".txt", subDir);
+        Files.write(createdFile2.toPath(), STAT_VALUE2.getBytes(), WRITE);
+
+        Method createAggregateStatFile = statsController.getClass().getDeclaredMethod("createAggregateStatFile", File.class);
+        createAggregateStatFile.setAccessible(true);
+        createAggregateStatFile.invoke(statsController, dir);
+
+        Method methodUnderTest = statsController.getClass()
+                .getDeclaredMethod("aggregateStats", File.class);
+        methodUnderTest.setAccessible(true);
+        methodUnderTest.invoke(statsController, dir);
+
+        StringBuilder aggregateStatFileContent = new StringBuilder();
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE);
+        aggregateStatFileContent.append(FileHelper.getNameWithoutExtension(createdFile2.getName()));
+        aggregateStatFileContent.append(",").append(STAT_VALUE2);
+
+        File aggregateStatFile = subDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File file, String s) {
+                return s.equals(subDir.getName() + Parameters.STATS_FILE_EXTENSION_WITH_DOT);
+            }
+        })[0];
+
+        Method readStatFromFile = statsController.getClass()
+                .getDeclaredMethod("readStatFromFile", File.class);
+        readStatFromFile.setAccessible(true);
+        String readAggregateStatsFileContent = (String) readStatFromFile.invoke(statsController, aggregateStatFile);
+
+        Assert.assertTrue("Content of aggregated file is wrong",
+                readAggregateStatsFileContent.equals(aggregateStatFileContent.toString()));
     }
 
 }
